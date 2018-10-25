9ccae0344538f8270121bc12744f10e8bc04c64d
Dedmen Miller <dedmen@dedmen.de>
2018-10-25 20:50:11 +0200

    show full path

--- BrofilerApp/Data/EventDescription.cs
+++ BrofilerApp/Data/EventDescription.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,7 +19,7 @@ namespace Profiler.Data
 			{
 				int index = File.LastIndexOfAny(new char[] { '\\', '/' });
 				ShortName = index != -1 ? File.Substring(index + 1) : File;
-				ShortPath = String.Format("{0}:{1}", ShortName, Line);
+				ShortPath = String.Format("{0}:{1}", File, Line);
 			}
 		}
 
